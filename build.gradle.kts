6e7ebe23f1c2f34c9b2559e640d7251b7265acb3
GiLik154 <okk080200@gamil.com>
2026-05-03 13:08:11 +0900

    chore: Spring Boot 스타터 라이브러리 빌드 골격 구성

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    `java-library`
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "io.github.jpamssqlhints"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.1")
+    }
+}
+
+dependencies {
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
+    compileOnly("org.springframework.boot:spring-boot-starter-aop")
+    compileOnly("org.hibernate.orm:hibernate-core")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-aop")
+    testImplementation("com.h2database:h2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("javadocJar") {
+    from(tasks.named("javadoc"))
+}
